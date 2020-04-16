455c6253e81d36f2635e72a0921c049fd3a0134c
Fly me to the moon <git@bigly.dog>
2020-04-16 03:57:07 -0700

    white space

--- lua/workspace/navigation.lua
+++ lua/workspace/navigation.lua
@@ -11,12 +11,16 @@ navigation = function ()
   local wild = function ()
     -- ui for cmd auto complete
     bindings.set("wildmenu")
+
     -- auto complete menu
     bindings.set("wildmode", "longest,list,full")
+
     -- hide file history
     bindings.set("complete", "i", [[-=]])
+
     -- show command history
     bindings.set("showcmd")
+
     -- more history
     bindings.set("history", 10000)
   end
