cd31f2a1e52e15d86f55d215de82d299e9a2ed38
Fly me to the moon <git@bigly.dog>
2020-04-16 20:56:58 -0700

    decrease keystroke time

---
+++ lua/workspace/help.lua
@@ -0,0 +1,18 @@
+local bindings = require "libs/bindings"
+
+
+local plugins = {}
+local defer = {}
+
+--#################### Help Region ####################
+
+local help_keys = function ()
+  bindings.map.normal("<leader>h", ":help ")
+end
+table.insert(defer, help_keys)
+
+
+return {
+  plugins = plugins,
+  defer = defer,
+}
