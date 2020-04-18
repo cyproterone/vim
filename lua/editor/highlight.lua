d5a941bc2c6eb7842bf0e14909af66bb263d6f71
Fly me to the moon <git@bigly.dog>
2020-04-17 21:27:15 -0700

    highlight copied region

--- lua/editor/highlight.lua
+++ lua/editor/highlight.lua
@@ -31,6 +31,16 @@ end
 table.insert(defer, line_number)
 
 
+-- highlight copied region
+table.insert(plugins, "machakann/vim-highlightedyank")
+local copy_highlight = function ()
+
+  bindings.let("highlightedyank_highlight_duration", 1000)
+
+end
+table.insert(defer, copy_highlight)
+
+
 -- focus on region
 table.insert(plugins, "junegunn/limelight.vim")
 local focus = function ()
