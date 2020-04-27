53449d8ec5ac78453a7f0786d2a51a3d89582837
Fly me to the moon <git@bigly.dog>
2020-04-27 00:00:48 -0700

    remove dup msg

--- lua/modes/command.lua
+++ lua/modes/command.lua
@@ -11,12 +11,6 @@ local keys = function ()
 
   bindings.map.command("<C-q>", "<Esc>")
 
-  -- emacs begining of line
-  bindings.map.command("<C-a>", "<Home>")
-
-  -- emacs end of line
-  bindings.map.command("<C-e>", "<End>")
-
   -- enable paste
   bindings.map.command("<C-v>", [[<C-r>"]])
 
