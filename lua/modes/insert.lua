f6bc00be2396673e175b20fa8322dd63051c271d
Fly me to the moon <git@bigly.dog>
2020-04-27 22:43:11 -0700

    auto centre on insert and movement

--- lua/modes/insert.lua
+++ lua/modes/insert.lua
@@ -21,6 +21,18 @@ end
 table.insert(defer, cursor_cross)
 
 
+-- auto centering on entering insertion mode
+local auto_centre = function ()
+
+  -- auto move cursor to center
+  bindings.auto{ group = "auto_centering",
+                 events = "InsertEnter",
+                 exec = "normal! zz", }
+
+end
+table.insert(defer, auto_centre)
+
+
 return {
   plugins = plugins,
   defer = defer,
--- lua/modes/multi.lua
+++ lua/modes/multi.lua
@@ -38,6 +38,28 @@ end
 table.insert(defer, remap_keys)
 
 
+local movement = function ()
+
+  -- centre on search result
+  bindings.map.normal("n", "nzz")
+  bindings.map.normal("N", "Nzz")
+
+
+  -- centre on up down
+  bindings.map.normal("j", "jzz")
+  bindings.map.normal("k", "kzz")
+  bindings.map.normal("<Up>", "<Up>zz")
+  bindings.map.normal("<Down>", "<Down>zz")
+
+
+  -- centre on paragraph
+  bindings.map.normal("{", "{zz")
+  bindings.map.normal("}", "}zz")
+
+end
+table.insert(defer, movement)
+
+
 return {
   plugins = plugins,
   defer = defer,
