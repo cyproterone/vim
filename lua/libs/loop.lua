930dbf027520f25b48092a9f14d8c83122690003
Fly me to the moon <git@bigly.dog>
2020-04-29 05:30:31 -0700

    added event loop

--- lua/libs/loop.lua
+++ lua/libs/loop.lua
@@ -4,47 +4,62 @@
 
 local loop = vim.loop
 
-local spawn = function (shell, stream)
+local spawn = function (shell, stream, cb)
 
   local stdin = loop.new_pipe()
   local stdout = loop.new_pipe()
   local stderr = loop.new_pipe()
+  local opts = {stdio = {stdin, stdout, stderr}}
+  local process
+  local out, err = {}, {}
+  local called = false
+  local handles = {stdin, stdout, stderr, process}
 
-  local on_write_end = function (err)
-    loop.close(stdin)
+  local call = function (err, res)
+    if called then
+      return
+    end
+    called = true
+    for _, handle in ipairs(handles) do
+      pcall(loop.close, handle)
+    end
+    cb(err, res)
   end
 
   local on_out = function (err, data)
-    if not data then
-      loop.close(stdout)
+    if data then
+      table.insert(out, data)
+    end
+    if err then
+      call(err)
     end
   end
 
   local on_err = function (err, data)
-    if not data then
-      loop.close(stderr)
+    if data then
+      table.insert(err, data)
+    end
+    if err then
+      call(err)
     end
   end
 
-  local process
   local on_exit = function (code, signal)
-    loop.close(process)
+    call(nil, {stdout = out, stderr = err})
   end
 
-  local opts = {stdio = {stdin, stdout, stderr}}
-  process = assert(loop.spawn(shell, opts, on_spawn))
+  process = loop.spawn(shell, opts, on_spawn)
+  if not process then
+    call("Error! - failed to spawn: " .. shell)
+    return
+  end
 
   loop.read_start(stdout, on_out)
   loop.read_start(stderr, on_err)
-
-  if stream
-  then
+  if stream then
     loop.write(stdin, stream)
   end
-
-  loop.shutdown(stdin, on_write_end)
-
-  loop.close(process, on_exit)
+  loop.shutdown(stdin)
 
 end
 
