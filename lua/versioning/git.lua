cbc0fadc533286149ac04189526705e0ec21d7c9
Fly me to the moon <git@bigly.dog>
2020-05-03 15:04:18 -0700

    remove gv

--- lua/versioning/git.lua
+++ lua/versioning/git.lua
@@ -4,8 +4,5 @@ local registry = require "libs/registry"
 
 --#################### Git Region ####################
 
--- view git history
-registry.install("junegunn/gv.vim")
-
 -- git in general
-registry.install("tpope/vim-fugitive")
+-- registry.install("tpope/vim-fugitive")
