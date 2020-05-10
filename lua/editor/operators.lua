bee45d6dc2dd6f4d42334c0c6d80d93ee7254a3f
Fly me to the moon <git@bigly.dog>
2020-05-09 22:51:46 -0700

    added gr for replace

---
+++ lua/editor/operators.lua
@@ -0,0 +1,7 @@
+local bindings = require "libs/bindings"
+local registry = require "libs/registry"
+
+
+--#################### Operators Region ####################
+
+registry.install("inkarkat/vim-ReplaceWithRegister")
