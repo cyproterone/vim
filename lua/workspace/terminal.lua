ad5c81b66c5cf1c295c42986e6972a76afba36a9
Fly me to the moon <git@bigly.dog>
2020-05-16 13:33:13 -0700

    term apperance

--- lua/workspace/terminal.lua
+++ lua/workspace/terminal.lua
@@ -4,6 +4,22 @@ local registry = require "libs/registry"
 
 --#################### Terminal Region ####################
 
+local apperance = function ()
+
+  -- set window options
+  local win_ui = function ()
+    local buf = bindings.call("expand", {"<abuf>"})
+    local win = bindings.call("bufwinid", {tonumber(buf)})
+    bindings.win.set(win, "signcolumn", "no")
+    bindings.win.set(win, "number", false)
+  end
+
+  registry.auto("TermOpen", win_ui)
+
+end
+registry.defer(apperance)
+
+
 local hotkeys = function ()
 
   -- leave terminal
