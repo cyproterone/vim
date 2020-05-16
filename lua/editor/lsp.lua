e559cf3a1e5550008036734bef44b638bfe561a3
Fly me to the moon <git@bigly.dog>
2020-05-16 03:20:17 -0700

    added some hotkeys

--- lua/editor/lsp.lua
+++ lua/editor/lsp.lua
@@ -7,6 +7,21 @@ local registry = require "libs/registry"
 registry.install("neovim/nvim-lsp")
 local lsp_recommends = function ()
 
+  bindings.map.normal("gd", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+  bindings.map.normal("gD", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+
+  bindings.map.normal("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+
+  bindings.map.normal("<Leader>cd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  bindings.map.normal("<Leader>cD", "<cmd>lua vim.lsp.buf.references()<CR>")
+
+  bindings.map.normal("<Leader>ck", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+  bindings.map.normal("<Leader>ct", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+
+  bindings.map.normal("<Leader>cs", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
+  bindings.map.normal("<Leader>cS", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
+
+  bindings.map.normal("<Leader>cS", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
 
 end
 registry.defer(lsp_recommends)
