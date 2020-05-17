a6359ac31c8e1ef398b005d1db554ecf770f7b9d
Fly me to the moon <git@bigly.dog>
2020-05-16 19:10:28 -0700

    magik

--- lua/editor/lsp.lua
+++ lua/editor/lsp.lua
@@ -12,13 +12,13 @@ local lsp_recommends = function ()
   bindings.map.normal("gd", "<cmd>lua vim.lsp.buf.declaration()<CR>")
   bindings.map.normal("gD", "<cmd>lua vim.lsp.buf.implementation()<CR>")
 
+  bindings.map.normal("gs", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  bindings.map.normal("gS", "<cmd>lua vim.lsp.buf.references()<CR>")
+
   bindings.map.normal("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
 
-  bindings.map.normal("<Leader>cd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-  bindings.map.normal("<Leader>cD", "<cmd>lua vim.lsp.buf.references()<CR>")
-
-  bindings.map.normal("<Leader>ck", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-  bindings.map.normal("<Leader>ct", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  bindings.map.normal("<Leader>cd", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+  bindings.map.normal("<Leader>cD", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
 
   bindings.map.normal("<Leader>cs", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
   bindings.map.normal("<Leader>cS", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
