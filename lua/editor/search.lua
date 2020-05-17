c004d2b3359519d9c1ef91872c7308309d2f328d
Fly me to the moon <git@bigly.dog>
2020-05-16 17:29:32 -0700

    fzf

--- lua/editor/search.lua
+++ lua/editor/search.lua
@@ -46,19 +46,15 @@ registry.defer(improved_search)
 
 -- get selection
 local fd_select = function (type)
-  local norm = [["]]
-  local reg = vim.fn.getreg(norm)
   local m = api.nvim_get_mode()
   if m.mode == "v" then
-    bindings.exec[[exe "normal! gvy"]]
+    bindings.exec[[exe "norm! gv\"zy"]]
   elseif type == "line" then
-    bindings.exec[[exe "normal! '[V']y"]]
+    bindings.exec[[exe "norm! '[V']\"zy"]]
   else
-    bindings.exec[[exe "normal! `[v`]y"]]
+    bindings.exec[[exe "norm! `[v`]\"zy"]]
   end
-  local selec = vim.fn.getreg(norm)
-  vim.fn.setreg(norm, reg)
-  return selec
+  return vim.fn.getreg("z", reg)
 end
 
 
@@ -78,13 +74,19 @@ registry.defer(magic)
 
 local find = function ()
 
-  lua_fd = function (type)
+  lua_op_fzf = function (type)
     local selec = fd_select(type)
-    print(selec)
-    -- bindings.exec[[execute "normal! :BLines \<C-r>\""]]
+    bindings.exec[[exe "norm! :BLines \<C-r>z\<CR>"]]
   end
 
-  bindings.map.normal("gf", ":set opfunc=v:lua.lua_fd<CR>g@")
+  lua_op_rg = function (type)
+    local selec = fd_select(type)
+    bindings.exec[[exe "norm! :Rg \<C-r>z\<CR>"]]
+  end
+
+  bindings.map.normal("gf", ":set opfunc=v:lua.lua_op_fzf<CR>g@")
+  bindings.map.normal("gf", ":set opfunc=v:lua.lua_op_rg<CR>g@")
+
   bindings.map.visual("gf", ":call v:lua.lua_fd()<CR>")
 
 end
