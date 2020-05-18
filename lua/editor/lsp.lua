0ec4d1cfaa41ab337a279e81b37a7f535c463626
Fly me to the moon <git@bigly.dog>
2020-05-18 13:25:09 -0700

    added which key to g

--- lua/editor/lsp.lua
+++ lua/editor/lsp.lua
@@ -9,17 +9,21 @@ local lsp_recommends = function ()
 
   bindings.set("omnifunc", "v:lua.vim.lsp.omnifunc")
 
+  -- go P
   bindings.map.normal("gp", "<cmd>lua vim.lsp.buf.definition()<CR>")
   bindings.map.normal("gP", "<cmd>lua vim.lsp.buf.references()<CR>")
 
+  -- go H
   bindings.map.normal("gh", "<cmd>lua vim.lsp.buf.declaration()<CR>")
   bindings.map.normal("gH", "<cmd>lua vim.lsp.buf.implementation()<CR>")
 
   bindings.map.normal("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
 
+  -- go U
   bindings.map.normal("gu", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
   bindings.map.normal("gU", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
 
+  -- go L
   bindings.map.normal("gl", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
   bindings.map.normal("gL", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
 
--- lua/workspace/help.lua
+++ lua/workspace/help.lua
@@ -9,6 +9,8 @@ local help = function ()
 
   bindings.map.normal("<Leader>", ":WhichKey '<Space>'<CR>")
 
+  bindings.map.normal("g", ":WhichKey 'g'<CR>")
+
   bindings.map.normal("[", ":WhichKey '['<CR>")
   bindings.map.normal("]", ":WhichKey ']'<CR>")
 
