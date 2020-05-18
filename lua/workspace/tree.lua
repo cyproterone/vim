f1cc6ff45d7486ac7f93e085a213ae36e767054f
Fly me to the moon <git@bigly.dog>
2020-05-17 23:42:12 -0700

    move nerdtree

--- lua/workspace/tree.lua
+++ lua/workspace/tree.lua
@@ -4,44 +4,50 @@ local registry = require "libs/registry"
 
 --#################### Tree Region ####################
 
-registry.install("preservim/nerdtree")
-local tree = function ()
+if fn.has("nvim") then
+  registry.install{"Shougo/defx.nvim", ["do"] = ":UpdateRemotePlugins"}
+  local tree = function ()
 
-  local file_home = var_home .. "/nerdtree"
+  end
+  registry.defer(tree)
+else
+  registry.install("preservim/nerdtree")
+  local tree = function ()
 
-  -- show hiddenfiles
-  bindings.let("NERDTreeShowHidden", true)
+    local file_home = var_home .. "/nerdtree"
 
-  -- buffer move with cursor
-  bindings.let("NERDTreeAutoCenter", true)
+    -- show hiddenfiles
+    bindings.let("NERDTreeShowHidden", true)
 
-  -- change cwd on root move
-  bindings.let("NERDTreeChDirMode", 2)
+    -- buffer move with cursor
+    bindings.let("NERDTreeAutoCenter", true)
 
-  -- remove buffer on file delete
-  bindings.let("NERDTreeAutoDeleteBuffer", true)
+    -- change cwd on root move
+    bindings.let("NERDTreeChDirMode", 2)
 
-  -- bookmark file location
-  bindings.let("NERDTreeBookmarksFile", file_home .. "/nerdtree_bookmarks")
+    -- remove buffer on file delete
+    bindings.let("NERDTreeAutoDeleteBuffer", true)
 
-  -- keybindings
-  bindings.map.normal("<Leader>i", ":NERDTreeToggle<CR>")
+    -- bookmark file location
+    bindings.let("NERDTreeBookmarksFile", file_home .. "/nerdtree_bookmarks")
 
-  -- nerdtree bindings
-  bindings.let("NERDTreeMapActivateNode", "<Tab>")
-  bindings.let("NERDTreeMapPreview", "d")
+    -- keybindings
+    bindings.map.normal("<Leader>i", ":NERDTreeToggle<CR>")
 
+    -- nerdtree bindings
+    bindings.let("NERDTreeMapActivateNode", "<Tab>")
+    bindings.let("NERDTreeMapPreview", "d")
+
+  end
+  registry.defer(tree)
+
+
+  registry.install("Xuyuanp/nerdtree-git-plugin")
+  registry.install("tiagofumo/vim-nerdtree-syntax-highlight")
+  local pretty_tree = function ()
+
+    bindings.let("NERDTreeHighlightFolders", true)
+
+  end
+  registry.defer(pretty_tree)
 end
-registry.defer(tree)
-
-
---#################### Tree Region ####################
-
-registry.install("Xuyuanp/nerdtree-git-plugin")
-registry.install("tiagofumo/vim-nerdtree-syntax-highlight")
-local pretty_tree = function ()
-
-  bindings.let("NERDTreeHighlightFolders", true)
-
-end
-registry.defer(tree)
