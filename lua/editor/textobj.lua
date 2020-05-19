44b1bf475d43b3474bebf383734c06fc3c79a43d
Fly me to the moon <git@bigly.dog>
2020-05-19 15:33:40 -0700

    remove expand region

--- lua/editor/textobj.lua
+++ lua/editor/textobj.lua
@@ -4,11 +4,6 @@ local registry = require "libs/registry"
 
 --#################### Text Object Region ####################
 
--- expand text object selection
-registry.install("terryma/vim-expand-region")
--- "+" to grow ,"-" to shrink
-
-
 registry.install("kana/vim-textobj-user")
 registry.install("kana/vim-textobj-entire")
 registry.install("kana/vim-textobj-line")
