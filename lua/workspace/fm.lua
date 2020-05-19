05b4e0b013af212058cb20fcbf0fd4fe4999c9a5
Fly me to the moon <git@bigly.dog>
2020-05-19 00:21:31 -0700

    holy crap the ranger plugin

--- lua/workspace/fm.lua
+++ lua/workspace/fm.lua
@@ -3,17 +3,28 @@ local bindings = require "libs/bindings"
 local registry = require "libs/registry"
 
 
---#################### Tree Region ####################
+--#################### File Manager Region ####################
 
 if bindings.executable("ranger") then
+
+  registry.install{"kevinhwang91/rnvimr", ["do"] = "make sync"}
   local tree = function ()
 
+    -- preview on root
     local ranger1 = " --cmd='set preview_files=true'"
     local ranger2 = " --cmd='set use_preview_script=true'"
     local ranger = "ranger" .. ranger1 .. ranger2
+    bindings.let("rnvimr_ranger_cmd", ranger)
+
+
+    -- replace netrw
+    bindings.let("rnvimr_ex_enable", true)
+    -- wipe deleted buf
+    bindings.let("rnvimr_bw_enable", true)
+
 
     -- keybindings
-    bindings.map.normal("<Leader>y", ":FloatermNew " .. ranger .. "<CR>")
+    bindings.map.normal("<Leader>i", ":RnvimrToggle<CR>")
 
   end
   registry.defer(tree)
