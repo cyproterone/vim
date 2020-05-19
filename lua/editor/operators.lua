4938f941a1bb142412e7ec5473bdf368d20cb150
Fly me to the moon <git@bigly.dog>
2020-05-19 02:18:38 -0700

    added recur open all

--- lua/editor/operators.lua
+++ lua/editor/operators.lua
@@ -12,3 +12,12 @@ registry.install("tpope/vim-surround")
 
 -- gc :: go comment
 registry.install("tpope/vim-commentary")
+
+-- ga :: go align
+registry.install("junegunn/vim-easy-align")
+local align = function ()
+
+  bindings.map.nv("ga", "<Plug>(EasyAlign)", {noremap = false})
+
+end
+registry.defer(align)
