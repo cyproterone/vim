74b0a3ca6c5026383a623f876278288e10fb0a6e
Fly me to the moon <git@bigly.dog>
2020-05-19 02:55:24 -0700

    remove ranger

--- lua/workspace/fm.lua
+++ lua/workspace/fm.lua
@@ -5,32 +5,6 @@ local registry = require "libs/registry"
 
 --#################### File Manager Region ####################
 
-if bindings.executable("ranger") then
-
-  registry.install{"kevinhwang91/rnvimr", ["do"] = "make sync"}
-  local tree = function ()
-
-    -- preview on root
-    local ranger1 = " --cmd='set preview_files=true'"
-    local ranger2 = " --cmd='set use_preview_script=true'"
-    local ranger = "ranger" .. ranger1 .. ranger2
-    bindings.let("rnvimr_ranger_cmd", ranger)
-
-
-    -- replace netrw
-    bindings.let("rnvimr_ex_enable", true)
-    -- wipe deleted buf
-    bindings.let("rnvimr_bw_enable", true)
-
-
-    -- keybindings
-    bindings.map.normal("<Leader>y", ":RnvimrToggle<CR>")
-
-  end
-  registry.defer(tree)
-end
-
-
 if bindings.has("nvim") then
 
   registry.install{"Shougo/defx.nvim", ["do"] = ":UpdateRemotePlugins"}
