3e64508bb55993ee2014853cf3a045d3d81dfbab
Fly me to the moon <git@bigly.dog>
2020-05-19 18:54:50 -0700

    sort lines working

--- lua/editor/format.lua
+++ lua/editor/format.lua
@@ -6,16 +6,26 @@ local registry = require "libs/registry"
 
 local sort = function ()
 
+  local p_marks = function (type)
+    local m1, m2 = unpack(type and {"[", "]"} or {"<", ">"})
+    local r1, c1 = unpack(api.nvim_buf_get_mark(0, m1))
+    local r2, c2 = unpack(api.nvim_buf_get_mark(0, m2))
+    return r1, c1, r2, c2
+  end
+
   lua_sort_lines = function (type)
-    if not type then
-      return
-    elseif type == "line" then
-      return
-    end
+    local r1, _, r2, _ = p_marks(type)
+    -- vim has mixed indexing
+    r1, r2 = r1 - 1, r2 - 1 + 1
+    local lines = api.nvim_buf_get_lines(0, r1, r2, true)
+    table.sort(lines, function (a, b)
+      return vim.stricmp(a, b) < 0
+    end)
+    api.nvim_buf_set_lines(0, r1, r2, true, lines)
   end
 
   bindings.map.normal("gs", ":set opfunc=v:lua.lua_sort_lines<CR>g@")
-  bindings.map.visual("gs", "<Esc>:v:lua.lua_sort_lines()<CR>")
+  bindings.map.visual("gs", "<Esc>:lua lua_sort_lines()<CR>")
 
 end
 registry.defer(sort)
