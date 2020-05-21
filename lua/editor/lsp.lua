41238ab6effecac013e190fe5959e63d57a476fb
Fly me to the moon <git@bigly.dog>
2020-05-21 11:55:15 -0700

    more reasonable lsp keybinds

--- lua/editor/lsp.lua
+++ lua/editor/lsp.lua
@@ -9,23 +9,20 @@ local lsp_recommends = function ()
 
   bindings.set("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  -- go P
+  bindings.map.normal("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  bindings.map.normal("L", "<cmd>lua vim.lsp.buf.rename()<CR>")
+
   bindings.map.normal("gp", "<cmd>lua vim.lsp.buf.definition()<CR>")
   bindings.map.normal("gP", "<cmd>lua vim.lsp.buf.references()<CR>")
 
-  -- go H
-  bindings.map.normal("gh", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-  bindings.map.normal("gH", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  bindings.map.normal("gl", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+  bindings.map.normal("gL", "<cmd>lua vim.lsp.buf.implementation()<CR>")
 
-  bindings.map.normal("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  bindings.map.normal("go", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+  bindings.map.normal("gO", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
 
-  -- go U
-  bindings.map.normal("gu", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-  bindings.map.normal("gU", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-
-  -- go L
-  bindings.map.normal("gl", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
-  bindings.map.normal("gL", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
+  bindings.map.normal("gu", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
+  bindings.map.normal("gU", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
 
 end
 registry.defer(lsp_recommends)
