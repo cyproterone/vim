a46f69e0e9be66aead45049ca675115334e820ec
Fly me to the moon <git@bigly.dog>
2020-05-21 17:23:35 -0700

    remove unnecessary code

--- lua/libs/io.lua
+++ lua/libs/io.lua
@@ -2,21 +2,6 @@
 --#################### IO Region ####################
 --#################### ######### ####################
 
-local exec = function (shell)
-  local stream = assert(io.popen(shell, "r"))
-  local acc = assert(stream:read("*a"))
-  stream:close()
-  return acc
-end
-
-
-local pipe = function (shell, text)
-  local stream = assert(io.popen(shell, "w"))
-  assert(stream:write(text))
-  stream:close()
-end
-
-
 local file_exists = function (filename)
   local file = io.open(filename, "r")
   if file then
@@ -28,7 +13,5 @@ end
 
 
 return {
-  exec = exec,
-  pipe = pipe,
   file_exists = file_exists,
 }
