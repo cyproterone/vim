c9424dcc9598d1db90ee0acaa9544b0efa825ca4
Fly me to the moon <git@bigly.dog>
2020-05-26 17:34:50 -0700

    golang

---
+++ lua/lang/go.lua
@@ -0,0 +1,21 @@
+--#################### ########### ####################
+--#################### Rust Region ####################
+--#################### ########### ####################
+local bindings = require "libs/bindings"
+local ftp = require "libs/ftp"
+
+
+local ft = {"go", "gomod"}
+
+
+local lsp = function ()
+  local lsp = require "nvim_lsp"
+  lsp.gopls.setup{}
+  lsp.gopls.manager.try_add()
+end
+
+
+local lang = function ()
+  lsp()
+end
+ftp.defer(ft, lang)
