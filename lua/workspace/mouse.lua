82e899e0efac875d2b985bc5b09d908cc014b9f8
Fly me to the moon <git@bigly.dog>
2020-05-29 12:23:26 -0700

    added more mouse opts

--- lua/workspace/mouse.lua
+++ lua/workspace/mouse.lua
@@ -10,5 +10,11 @@ local normal_mouse = function ()
   -- enable mouse in all modes
   bindings.set("mouse", "a")
 
+  -- right click behaviour
+  bindings.set("mousemodel", "popup_setpos")
+
+  -- doubleclick time
+  bindings.set("mousetime", 250)
+
 end
 registry.defer(normal_mouse)
