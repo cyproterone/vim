54929ef863baa15dac4a931d687e7b502a3a0b45
Fly me to the moon <git@bigly.dog>
2020-05-29 12:17:53 -0700

    "

--- lua/editor/cursors.lua
+++ lua/editor/cursors.lua
@@ -12,7 +12,7 @@ registry.install("matze/vim-move")
 registry.install("Raimondi/delimitMate")
 local match_cursor = function ()
 
-  bindings.set("matchpairs", [[<:>]], [[+=]])
+  bindings.set("matchpairs", "<:>", [[+=]])
 
 end
 registry.defer(match_cursor)
