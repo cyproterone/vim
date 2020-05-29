e243c405097fb5570ea8ed75a94d455bb243d469
Fly me to the moon <git@bigly.dog>
2020-05-29 10:29:50 -0700

    remove editor config

--- lua/workspace/proj.lua
+++
@@ -1,8 +0,0 @@
-local bindings = require "libs/bindings"
-local registry = require "libs/registry"
-
-
---#################### Project Region ####################
-
--- consistent coding style
-registry.install("editorconfig/editorconfig-vim")
