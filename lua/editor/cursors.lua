230aef5c84cf912718a13cff72df5266423fb959
Fly me to the moon <git@bigly.dog>
2020-06-06 13:42:41 -0700

    attempt at mv

--- lua/editor/cursors.lua
+++ lua/editor/cursors.lua
@@ -8,11 +8,37 @@ local registry = require "libs/registry"
 registry.install("matze/vim-move")
 local vim_move = function ()
 
-  bindings.map.normal("<M-Up>",   "<Plug>MoveLineUp",    {noremap = false})
-  bindings.map.normal("<M-Down>", "<Plug>MoveLineDown",  {noremap = false})
+  local get_visual = function ()
+    local r1, _ = unpack(api.nvim_buf_get_mark(0, "<"))
+    local r2, _ = unpack(api.nvim_buf_get_mark(0, ">"))
+    return r1, r2
+  end
 
-  bindings.map.visual("<M-Up>",   "<Plug>MoveBlockUp",   {noremap = false})
-  bindings.map.visual("<M-Down>", "<Plug>MoveBlockDown", {noremap = false})
+  local swap = function (visual, down, r1, r2)
+    if down then
+      local lines = api.nvim_buf_get_lines(0, r1, r2, true)
+      api.nvim_buf_set_lines(0, r1, r2, true, lines)
+    else
+      local lines = api.nvim_buf_get_lines(0, r1, r2, true)
+      api.nvim_buf_set_lines(0, r1, r2, true, lines)
+    end
+  end
+
+  lua_move = function (visual, down)
+    if visual then
+      local r1, r2 = get_visual()
+      swap(true, down, r1, r2)
+    else
+      local r = unpack(api.nvim_win_get_cursor(0))
+      swap(true, down, r, r)
+    end
+  end
+
+  bindings.map.normal("<M-Up>",   ":lua lua_move(false, false)<CR>")
+  bindings.map.normal("<M-Down>", ":lua lua_move(false, true)<CR>")
+
+  bindings.map.visual("<M-Up>",   "<Esc>:lua lua_move(true, false)<CR>")
+  bindings.map.visual("<M-Down>", "<Esc>:lua lua_move(true, true)<CR>")
 
 end
 registry.defer(vim_move)
