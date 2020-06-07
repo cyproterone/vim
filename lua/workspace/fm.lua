d6db946c36056dff2a616759632a8d71805ce08f
Fly me to the moon <git@bigly.dog>
2020-06-07 03:11:39 -0700

    luatree no follow

--- lua/workspace/fm.lua
+++ lua/workspace/fm.lua
@@ -7,11 +7,31 @@ local registry = require "libs/registry"
 registry.install("kyazdani42/nvim-tree.lua")
 registry.install("kyazdani42/nvim-web-devicons")
 
+local options = function ()
+
+  -- dont auto expand tree
+  vim.g.lua_tree_follow = 0
+
+end
+registry.defer(options)
+
+
 local key = function ()
 
   local remap = function ()
 
     bindings.map.normal("R", ":LuaTreeRefresh<CR>")
+
+    lv.tree_preview = function ()
+      local pos = api.nvim_win_get_cursor(0)
+      local tree = require "tree"
+      tree.on_keypress("edit")
+      bindings.exec[[LuaTreeClose]]
+      bindings.exec[[LuaTreeOpen]]
+      api.nvim_win_set_cursor(0, pos)
+    end
+
+    bindings.map.normal("<Tab>", ":lua lv.tree_preview()<CR>")
 
   end
   registry.auto("FileType", remap, "LuaTree")
