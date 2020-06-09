d8fcbd0f0157923cd5630457d226558a46702097
Fly me to the moon <git@bigly.dog>
2020-06-09 13:45:24 -0700

    goreplace!

--- lua/editor/paste.lua
+++ lua/editor/paste.lua
@@ -7,18 +7,29 @@ local registry = require "libs/registry"
 
 local go_replace = function ()
 
+
   lv.op_go_replace = function (selec)
     local r1, c1, r2, c2 = bindings.p_op_marks(selec)
-
+    r1, r2 = r1 - 1, r2 - 1
+    c1, c2 = c1 + 1, c2 + 1
+    local text = fn.getreg("*")
+    local lines = api.nvim_buf_get_lines(0, r1, r2 + 1, true)
+    local lst = r2 - r1 + 1
+    local pre = string.sub(lines[1], 1, c1 - 1)
+    local post = string.sub(lines[lst], c2 + 1, string.len(lines[lst]))
+    local replacement = pre .. text .. post
+    api.nvim_buf_set_lines(0, r1, r2 + 1, true, {replacement})
   end
 
   bindings.map.normal("gr", "<cmd>set opfunc=v:lua.lv.op_go_replace<CR>g@")
   bindings.map.visual("gr", "<Esc><cmd>lua lv.op_go_replace()<CR>")
 
+
+  lv.op_go_replace_line = function ()
+  end
+
+  bindings.map.normal("grr", "<cmd>lua lv.op_go_replace_line()<CR>")
+
 end
 registry.defer(go_replace)
-
-
--- gr :: go replace
--- registry.install("inkarkat/vim-ReplaceWithRegister")
 
