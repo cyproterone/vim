f08fe218c3d7248e16f5bc9f4787284c81473d0c
Fly me to the moon <git@bigly.dog>
2020-06-13 09:58:34 -0700

    split new lines

--- lua/editor/paste.lua
+++ lua/editor/paste.lua
@@ -6,6 +6,15 @@ local registry = require "libs/registry"
 
 local go_replace = function ()
 
+
+  local split_newline = function (str)
+    local new_lines = {}
+    for line in vim.gsplit(replacement, "\n", true) do
+      table.insert(new_lines, line)
+    end
+    return new_lines
+  end
+
   lv.op_go_replace = function (selec)
     local r1, c1, r2, c2 = bindings.p_op_marks(selec)
     r1, r2 = r1 - 1, r2 - 1
@@ -16,10 +25,7 @@ local go_replace = function ()
     local pre = string.sub(lines[1], 1, c1 - 1)
     local post = string.sub(lines[lst], c2 + 1, string.len(lines[lst]))
     local replacement = pre .. text .. post
-    local new_lines = {}
-    for line in vim.gsplit(replacement, "\n", true) do
-      table.insert(new_lines, line)
-    end
+    local new_lines = split_newline(replacement)
     api.nvim_buf_set_lines(0, r1, r2 + 1, true, new_lines)
   end
 
@@ -30,8 +36,9 @@ local go_replace = function ()
   lv.op_go_replace_line = function ()
     local r, _ = unpack(api.nvim_win_get_cursor(0))
     r = r - 1
-    local text = fn.getreg("*")
-    api.nvim_buf_set_lines(0, r, r + 1, true, {text})
+    local replacement = fn.getreg("*")
+    local new_lines = split_newline(replacement)
+    api.nvim_buf_set_lines(0, r, r + 1, true, new_lines)
   end
 
   bindings.map.normal("grr", "<cmd>lua lv.op_go_replace_line()<CR>")
