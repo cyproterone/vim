c9e5adf2e0c543d83bebb000d18ae217bfac5663
Fly me to the moon <git@bigly.dog>
2020-06-18 12:58:14 -0700

    use no wait

--- lua/modes/multi.lua
+++ lua/modes/multi.lua
@@ -7,8 +7,8 @@ local registry = require "libs/registry"
 local map_keys = function ()
 
   -- use @ to do ^
-  bindings.map.no("@", "^")
-  bindings.map.no("^", "@")
+  bindings.map.no("@", "^", {nowait = true})
+  bindings.map.no("^", "@", {nowait = true})
 
 end
 registry.defer(map_keys)
