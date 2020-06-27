873de223ffde10bf3230bc756bc083134aee73d2
Fly me to the moon <git@bigly.dog>
2020-06-27 02:00:55 -0700

    entire works

--- lua/editor/textobj.lua
+++ lua/editor/textobj.lua
@@ -5,7 +5,6 @@ local registry = require "libs/registry"
 --#################### Text Object Region ####################
 
 registry.install("kana/vim-textobj-user")
--- registry.install("kana/vim-textobj-entire")
 registry.install("kana/vim-textobj-line")
 registry.install("michaeljsmith/vim-indent-object")
 registry.install("wellle/targets.vim")
@@ -17,12 +16,15 @@ local entire = function ()
     local count = api.nvim_buf_line_count(0)
     local line = unpack(api.nvim_buf_get_lines(0, -2, -1, true))
     local len = string.len(line)
-    fn.setpos("'[", {0, 1, 1, 0})
-    fn.setpos("']", {0, count, len, 0})
-    print(count, len)
+    fn.setpos("'<", {0, 1, 1, 0})
+    fn.setpos("'>", {0, count, len, 0})
+    bindings.exec[[norm! gv]]
   end
 
-  bindings.map.operator("ae", "<cmd>lua lv.textobj_entire()<CR>", {silent=false})
+  bindings.map.operator("ie", "<cmd>lua lv.textobj_entire()<CR>")
+  bindings.map.operator("ae", "<cmd>lua lv.textobj_entire()<CR>")
+  bindings.map.visual("ie",   "<Esc><cmd>lua lv.textobj_entire()<CR>")
+  bindings.map.visual("ae",   "<Esc><cmd>lua lv.textobj_entire()<CR>")
 
 end
 registry.defer(entire)
