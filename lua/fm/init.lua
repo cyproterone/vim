ec537dc4de1be94080e45d11e8e5c9349654f57f
Fly me to the moon <git@bigly.dog>
2020-07-07 12:53:35 -0700

    :D

--- lua/fm/init.lua
+++ lua/fm/init.lua
@@ -5,10 +5,18 @@ local fs = require "libs/fs"
 local read_tree = function (root, depth)
   local rt = function (root, d)
     return a.sync(function ()
+      local ret = {}
+      if d > depth then
+        return ret
+      else
+        local contents = a.wait(fs.read_dir(root))
+        return contents
+      end
     end)
   end
-  return rt(root, 0)
+  return rt(root, depth)
 end
+
 
 a.sync(function ()
   local things = a.wait(read_tree(fs.cwd()))
