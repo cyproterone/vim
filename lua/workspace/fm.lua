85ea95d8a0a6feb8c95539899b13fa0ac601e465
Fly me to the moon <git@bigly.dog>
2020-07-30 00:36:17 -0700

    chad

--- lua/workspace/fm.lua
+++ lua/workspace/fm.lua
@@ -4,7 +4,7 @@ local registry = require "libs/registry"
 
 --#################### File Manager Region ####################
 
-registry.install("ms-jpq/fancy-fm", {["branch"] = "nvim", ["do"] = ":UpdateRemotePlugins"})
+registry.install("ms-jpq/chadtree", {["branch"] = "chad", ["do"] = ":UpdateRemotePlugins"})
 local keybind = function ()
 
   bindings.map.normal("<leader>v", "<cmd>FMopen<cr>")
