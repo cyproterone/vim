a214499e4d83993fc67c6e8507c2b5fd41f3e64f
Fly me to the moon <git@bigly.dog>
2020-08-07 16:42:33 -0700

    delimt

--- lua/editor/misc.lua
+++ lua/editor/misc.lua
@@ -5,4 +5,25 @@ local registry = require "libs/registry"
 --#################### Misc Region ####################
 
 -- auto parens
-registry.install("Raimondi/delimitMate")
+local delimit = function ()
+
+  local match_pairs = {
+    ["{"] = "{}",
+    ['"'] = '""',
+    ["'"] = "''"
+  }
+
+  local de = function ()
+    local char = match_pairs[vim.v.char]
+    if char then
+      vim.v.char = char
+      local row, col = unpack(api.nvim_win_get_cursor(0))
+      vim.schedule(function ()
+        api.nvim_win_set_cursor(0, {row, col + 1})
+      end)
+    end
+  end
+  registry.auto("InsertCharPre", de)
+
+end
+registry.defer(delimit)
