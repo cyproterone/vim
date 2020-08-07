fa30f0a26b79ccebeafd6821312e212503caf1ea
Fly me to the moon <git@bigly.dog>
2020-08-07 11:32:22 -0700

    higher debug level

--- lua/workspace/fm.lua
+++ lua/workspace/fm.lua
@@ -7,7 +7,7 @@ local registry = require "libs/registry"
 registry.install("ms-jpq/chadtree", {["branch"] = "chad", ["do"] = ":UpdateRemotePlugins"})
 local keybind = function ()
 
-  vim.g.chadtree_settings = {logging_level = "DEBUG"}
+  vim.g.chadtree_settings = {logging_level = "INFO"}
   bindings.map.normal("<leader>v", "<cmd>CHADopen<cr>")
 
 end
