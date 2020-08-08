57cfb0883c85840c841ad5f664c332c241e772dd
Fly me to the moon <git@bigly.dog>
2020-08-08 16:30:56 -0700

    delimt

--- lua/editor/delimiters.lua
+++ lua/editor/delimiters.lua
@@ -4,46 +4,47 @@ local registry = require "libs/registry"
 
 --#################### Misc Region ####################
 
+registry.install("Raimondi/delimitMate")
 -- auto parens
-local delimit = function ()
+-- local delimit = function ()
 
-  local match_pairs = {
-    ["{"] = "}",
-    ["["] = "]",
-    ["("] = ")",
-    ['"'] = '"',
-    ["'"] = "'",
-    ["`"] = "`",
-  }
+--   local match_pairs = {
+--     ["{"] = "}",
+--     ["["] = "]",
+--     ["("] = ")",
+--     ['"'] = '"',
+--     ["'"] = "'",
+--     ["`"] = "`",
+--   }
 
-  local match_cond = function (lhs, rhs)
-    if lhs ~= rhs then
-      return true
-    else
-      local line = api.nvim_get_current_line()
-      local count = 0
-      for i = 1, #line do
-        local c = string.sub(line, i, i)
-        if c == rhs then
-          count = count + 1
-        end
-      end
-      return count % 2 == 0
-    end
-  end
+--   local match_cond = function (lhs, rhs)
+--     if lhs ~= rhs then
+--       return true
+--     else
+--       local line = api.nvim_get_current_line()
+--       local count = 0
+--       for i = 1, #line do
+--         local c = string.sub(line, i, i)
+--         if c == rhs then
+--           count = count + 1
+--         end
+--       end
+--       return count % 2 == 0
+--     end
+--   end
 
-  local de = function ()
-    local lhs = vim.v.char
-    local rhs = match_pairs[vim.v.char]
-    if rhs and match_cond(lhs, rhs) then
-      vim.v.char = lhs .. rhs
-      local row, col = unpack(api.nvim_win_get_cursor(0))
-      vim.schedule(function ()
-        api.nvim_win_set_cursor(0, {row, col + 1})
-      end)
-    end
-  end
-  registry.auto("InsertCharPre", de)
+--   local de = function ()
+--     local lhs = vim.v.char
+--     local rhs = match_pairs[vim.v.char]
+--     if rhs and match_cond(lhs, rhs) then
+--       vim.v.char = lhs .. rhs
+--       local row, col = unpack(api.nvim_win_get_cursor(0))
+--       vim.schedule(function ()
+--         api.nvim_win_set_cursor(0, {row, col + 1})
+--       end)
+--     end
+--   end
+--   registry.auto("InsertCharPre", de)
 
-end
-registry.defer(delimit)
+-- end
+-- registry.defer(delimit)
