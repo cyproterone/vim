4966492f7cd51e5011361fe4769e5b462b0d5273
Fly me to the moon <git@bigly.dog>
2020-08-15 17:06:00 -0700

    trim ws

--- lua/editor/whitespace.lua
+++ lua/editor/whitespace.lua
@@ -1,5 +1,6 @@
 local bindings = require "libs/bindings"
 local registry = require "libs/registry"
+local std = require "libs/std"
 
 
 --#################### Whitespace Region ####################
@@ -41,11 +42,25 @@ local trailing_whitespace = function ()
     local m = api.nvim_get_mode()
     if m.mode ~= "n" then
       return
+    else
+      local row, col = unpack(api.nvim_win_get_cursor(0))
+      local lines = api.nvim_buf_get_lines(0, 0, -1, true)
+
+      local trimmable = true
+      for i in std.range(#lines, 1, -1) do
+        local line = lines[i]
+        local new_line = string.gsub(line, "%s$", "")
+        trimmable = trimmable and i > row and new_line == ""
+        if trimmable then
+          lines[i] = nil
+        elseif i ~= row then
+          lines[i] = new_line
+        end
+      end
+
+      api.nvim_buf_set_lines(0, 0, -1, true, lines)
+      api.nvim_win_set_cursor(0, {row, col})
     end
-    local pos = api.nvim_win_get_cursor(0)
-    bindings.exec[[%s/\v\s+$//e]]
-    bindings.exec[[%s/\v$\n*%$//e]]
-    api.nvim_win_set_cursor(0, pos)
   end
 
   registry.auto("BufWritePre", strip, "*", "silent undojoin |")
